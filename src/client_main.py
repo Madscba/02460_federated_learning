011d1660539417e9cb25d408219d8fb64b690947
s175548 <s175548@student.dtu.dk>
2022-03-24 15:00:05 +0100

    ability to override wandb config

--- src/client_main.py
+++ src/client_main.py
@@ -9,6 +9,8 @@ import argparse
 import os
 import wandb
 from train_test_utils import choose_train_fn, load_data
+from main_utils import parse_args, update_config
+
 
 warnings.filterwarnings("ignore", category=UserWarning)
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -27,8 +29,11 @@ def main(args):
     config=os.path.join(os.getcwd(),'src','config',args.configs)
     wandb.login(key='47304b319fc295d13e84bba0d4d020fc41bd0629')
     wandb.init(project="02460_federated_learning", entity="02460-federated-learning", group=experiment, config=config, mode=args.wandb_mode)
+    update_config(args)
     wandb.run.name = args.user+wandb.run.id
     wandb.run.save()
+
+
 
 
     # Load data (CIFAR-10)
@@ -43,43 +48,5 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--user', 
-                help='user ex f0000_14', default='f0000_14')
-    parser.add_argument('--wandb_mode', 
-                help='use "online" to log and sync with cloud', default='disabled')
-    parser.add_argument('--configs', default='config.yaml')
-    parser.add_argument('--experiment_id', default=None)
-    parser.add_argument('--wandb_username', default=None)
-    parser.add_argument("--dp_sgd",default=False)
-    parser.add_argument("--opacus", default=False, help="Set true to use opacus library")
-    parser.add_argument(
-        "--noise_multiplier",
-        type=float,
-        default=1.0,
-        metavar="S",
-        help="Noise multiplier",
-    )
-    parser.add_argument(
-        "-c",
-        "--max_grad_norm",
-        type=float,
-        default=1.0,
-        metavar="C",
-        help="Clip per-sample gradients to this norm",
-    )
-    parser.add_argument(
-        "--target_delta",
-        type=float,
-        default=1e-5,
-        metavar="D",
-        help="Target delta",
-    )
-    parser.add_argument(
-        "--sample_rate",
-        type=float,
-        default=0.01,
-        metavar="Q",
-        help="Sample rate (determined on server level)",
-    )
-    args = parser.parse_args()
+    args=parse_args(parser)
     main(args)
