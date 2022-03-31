4e5689119e2a5be8354cd9e84035b914bc38a014
s175549 <s175549@student.dtu.dk>
2022-03-31 13:32:08 +0200

    Update run_dp.sh

--- src/run_dp.sh
+++ src/run_dp.sh
@@ -36,8 +36,7 @@ sleep 3  # Sleep for 3s to give the server enough time to start
 
 while read user && (($n<=$N)); do
 	echo "Starting client: $n , name: $user"
-   	timeout 4m python src/client_main.py --user=${user} --wandb_mode="online" --experiment_id=$exp_id --wandb_username="johannes_boe" --configs=dp_sgd.yaml --epochs=$epoch_num --dataset_path='/work3/s173934/AdvML/02460_federated_learning/dataset/femnist'& 
-
+   	timeout 3m python src/client_main.py --user=${user} --wandb_mode="online" --experiment_id=$exp_id --wandb_username='johannes_boe' --configs=dp_sgd.yaml --epochs=$epoch_num --dataset_path='/work3/s173934/AdvML/02460_federated_learning/dataset/femnist'& 
 	if [ $(expr $n % 10) == 0 ] && [ $n>$n_wait ]; then
 		echo "sleeping for 120 sec" ##120 sec
 		sleep 120
