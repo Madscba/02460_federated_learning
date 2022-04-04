63d5500fbf159184e5ebc6d90fa4d569f3833ae9
s175549 <s175549@student.dtu.dk>
2022-04-04 21:13:35 +0200

    Update run_dp.sh

--- src/run_files/run_dp.sh
+++ src/run_files/run_dp.sh
@@ -21,7 +21,7 @@ filename='/work3/s173934/AdvML/02460_federated_learning/dataset/femnist/data/img
 n=1 #spawned_clients
 N=500 #amount of clients
 n_wait=9
-epoch_num=2
+epoch_num=5
 exp_id=$(date +"DP_%d%b%T")
 
 echo "starting bash script"
@@ -30,16 +30,16 @@ module load python3/3.8.0
 source /zhome/dd/4/128822/fl_362/bin/activate
 
 echo "Starting server"
-python src/server.py --strategy="DP_Fed" --experiment_id=$exp_id --wandb_username='johannes_boe' --configs=dp_sgd.yaml &
+python src/server.py --strategy="DP_Fed" --experiment_id=$exp_id --wandb_username='johannes_boe' --wandb_mode="online" --configs=dp_sgd.yaml &
 sleep 3  # Sleep for 3s to give the server enough time to start
 
 
 while read user && (($n<=$N)); do
 	echo "Starting client: $n , name: $user"
-   	timeout 4m python src/client_main.py --user=${user} --experiment_id=$exp_id --wandb_username='johannes_boe' --configs=dp_sgd.yaml --epochs=$epoch_num --dataset_path='/work3/s173934/AdvML/02460_federated_learning/dataset/femnist'& 
+   	timeout 2m python src/client_main.py --user=${user} --experiment_id=$exp_id --wandb_username='johannes_boe' --wandb_mode="online" --configs=dp_sgd.yaml --epochs=$epoch_num --dataset_path='/work3/s173934/AdvML/02460_federated_learning/dataset/femnist'& 
 	if [ $(expr $n % 10) == 0 ] && [ $n>$n_wait ]; then
-		echo "sleeping for 120 sec" ##120 sec
-		sleep 120
+		echo "sleeping for 60 sec" ##60 sec
+		sleep 60
 	fi
 	n=$((n+1))
 done < $filename
