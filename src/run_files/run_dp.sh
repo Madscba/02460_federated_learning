86ea88062c7ed075b3553b9874cffa01c453b316
s175549 <s175549@student.dtu.dk>
2022-04-04 11:14:31 +0200

    Update run_dp.sh

--- src/run_files/run_dp.sh
+++ src/run_files/run_dp.sh
@@ -30,13 +30,13 @@ module load python3/3.8.0
 source /zhome/dd/4/128822/fl_362/bin/activate
 
 echo "Starting server"
-python src/server.py --strategy="DP_Fed" --experiment_id=$exp_id --wandb_username='johannes_boe' --wandb_mode="online" --configs=dp_sgd.yaml &
+python src/server.py --strategy="DP_Fed" --experiment_id=$exp_id --wandb_username='johannes_boe' --configs=dp_sgd.yaml &
 sleep 3  # Sleep for 3s to give the server enough time to start
 
 
 while read user && (($n<=$N)); do
 	echo "Starting client: $n , name: $user"
-   	timeout 4m python src/client_main.py --user=${user} --wandb_mode="online" --experiment_id=$exp_id --wandb_username='johannes_boe' --configs=dp_sgd.yaml --epochs=$epoch_num --dataset_path='/work3/s173934/AdvML/02460_federated_learning/dataset/femnist'& 
+   	timeout 4m python src/client_main.py --user=${user} --experiment_id=$exp_id --wandb_username='johannes_boe' --configs=dp_sgd.yaml --epochs=$epoch_num --dataset_path='/work3/s173934/AdvML/02460_federated_learning/dataset/femnist'& 
 	if [ $(expr $n % 10) == 0 ] && [ $n>$n_wait ]; then
 		echo "sleeping for 120 sec" ##120 sec
 		sleep 120
