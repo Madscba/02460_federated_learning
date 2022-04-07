9e8f18b1edd6839bba21a782e6ab15ee63c961ed
KarlUlbaek <karl.ulbaek@hotmail.com>
2022-04-07 16:53:51 +0200

    fixing merge (hopefully)

--- src/global_model_eval.py
+++ src/global_model_eval.py
@@ -3,16 +3,25 @@ from client_dataset import FemnistDataset
 import torchvision.transforms as transforms
 import torch
 from model import Net
-from tqdm import tqdm
+from collections import OrderedDict
 
 def global_model_eval(state_dict ="saved_models/Qfed_manual_state_dict.pt",
                       user_names_test_file = "dataset/femnist/data/img_lab_by_user/user_names_test.txt",
+                      parameters = None,
                       num_test_clients = None,  # this is the indexing of the list so None means all
-                      get_loss = False):
+                      get_loss = False
+                      ):
+
     DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     net = Net().to(DEVICE)
-    net.load_state_dict(torch.load(state_dict))
-    loss_func = torch.nn.CrossEntropyLoss()
+    if parameters:
+        params_dict = zip(net.state_dict().keys(), parameters)
+        state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+        net.load_state_dict(state_dict, strict=True)
+    else:
+        net.load_state_dict(torch.load(state_dict))
+
+    if get_loss: loss_func = torch.nn.CrossEntropyLoss()
 
     with open(user_names_test_file) as file:
         user_names_test = sorted([line.strip() for line in file][:num_test_clients])
@@ -27,7 +36,6 @@ def global_model_eval(state_dict ="saved_models/Qfed_manual_state_dict.pt",
             # set arbitrary big batch size such that we only get one batch
             # with all the data
             data_loader = DataLoader(dataset, batch_size=8000)
-
 
             for x, y in data_loader:
                 x, y = x.to(DEVICE), y.to(DEVICE)
