e3da9b2c1746a3422a139a900ecdddb5a6dfd6f2
s175549 <s175549@student.dtu.dk>
2022-04-07 12:48:43 +0200

    calculate epsilon

--- src/privacy_opt.py
+++ src/privacy_opt.py
@@ -8,34 +8,30 @@ class PrivacyAccount():
     def __init__(
             self,
             step: int = 0,
-            batch_size: int = None,
             sample_size: int = None,
             sample_rate: float = None,
             alphas: List = DEFAULT_ALPHAS,
             max_grad_norm: float = None,
             noise_multiplier: float = None,
-            noise_scale: float = None,
             target_delta: float = None):
 
         self.steps = step
         self.alphas = alphas
-        self.batch_size = batch_size
         self.sample_size = sample_size
         self.sample_rate = sample_rate
         self.noise_multiplier = noise_multiplier
-        self.noise_scale = noise_scale
         self.max_grad_norm = max_grad_norm
         self.target_delta = target_delta
-        self._set_sample_rate()
-
-    def step(self):
-        self.steps += 1
-
-    def _set_sample_rate(self):
-        if not self.sample_rate:
-            self.sample_rate = self.batch_size / self.sample_size
+        self._set_target_delta()
+    
+    def _set_target_delta(self):
+        if not self.target_delta:
+            self.target_delta = 0.1 * (1 / self.sample_size)
 
     def get_renyi_divergence(self):
+        sensitivity = self.max_grad_norm / (self.sample_size*self.sample_rate)
+        z = self.noise_multiplier/sensitivity
+
         rdp = torch.tensor(
             privacy_analysis.compute_rdp(
                 self.sample_rate, self.noise_multiplier, 1, self.alphas
@@ -43,7 +39,7 @@ class PrivacyAccount():
         )
         return rdp
 
-    def get_privacy_spent(self, target_delta: float = None):
+    def get_privacy_spent(self, target_delta: float = None, steps: int = None):
         """
         Computes the (epsilon, delta) privacy budget spent so far.
 
@@ -64,7 +60,7 @@ class PrivacyAccount():
                 )
             target_delta = self.target_delta
         rdp = self.get_renyi_divergence() * self.steps
-        eps, best_alpha = privacy_analysis.get_privacy_spent(
+        eps, _ = privacy_analysis.get_privacy_spent(
             self.alphas, rdp, target_delta
         )
         return float(eps)
