f6e31a05cc4e2d44362ce35a8670aeb7529d1ccc
s175549 <s175549@student.dtu.dk>
2022-04-15 13:27:09 +0200

    update dp strategy

--- src/run_files/run_dp.sh
+++ src/run_files/run_dp.sh
@@ -7,7 +7,7 @@
 ##BSUB -R "select[model=XeonGold6126]"
 #BSUB -R "span[hosts=1]"
 #BSUB -M 4GB
-#BSUB -W 00:50 ##20 minutes (hh:mm)
+#BSUB -W 00:40 ##120 minutes (hh:mm)
 ###BSUB -B 
 #BSUB -N 
 #BSUB -o O_fl_%J.out 
@@ -26,7 +26,7 @@ rounds=1
 wandb_mode="online"
 exp_id='DP_E1'
 strategy='DP_Fed'
-batch_size=8
+batch_size=10
 
 echo "starting bash script"
 
@@ -48,12 +48,12 @@ python src/server_main.py --wandb_mode=$wandb_mode \
 --batch_size=$batch_size \
 --total_num_clients=$N \
 --rounds=$rounds&pid=$!
+
 sleep 3  # Sleep for 3s to give the server enough time to start
 
 
 while read user && (($n<=$N)) && ps -p $pid > /dev/null 2>&1; do
 	echo "Starting client: $n , name: $user"
-   	timeout 2m python src/client_main.py --user=${user} --experiment_id=$exp_id --wandb_username='johannes_boe' --wandb_mode="online" --configs=dp_sgd.yaml --epochs=$epoch_num --batch_size=$batch_size --dataset_path='/work3/s173934/AdvML/02460_federated_learning/dataset/femnist' & 
 	timeout 2m python src/client_main.py \
 	--seed=$n \
 	--experiment_id=$exp_id \
@@ -63,8 +63,11 @@ while read user && (($n<=$N)) && ps -p $pid > /dev/null 2>&1; do
 	--job_type="client_$strategy" \
 	--config=dp_sgd.yaml \
 	--epochs=$epoch_num \
+	--entity johannes_boe \
+	--api_key d9a0e4bbe478bc7e59b80f931b0281cb3501e8dd \
+	--wandb_project 02460_FL \
 	--batch_size=$batch_size \
-	 --dataset_path='/work3/s173934/AdvML/02460_federated_learning/dataset/femnist'&
+	--dataset_path='/work3/s173934/AdvML/02460_federated_learning/dataset/femnist'&
 	if [ $(expr $n % 10) == 0 ]; then
 		echo "sleeping for" $((30+5*$epoch_num)) 
 		sleep $((30+5*$epoch_num))
